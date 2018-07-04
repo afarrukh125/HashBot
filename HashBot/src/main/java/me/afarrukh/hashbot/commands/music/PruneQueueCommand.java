3a9b2baf98f17fb583b69230b2edad49b2ac53f2
afarrukh125 <abdullahfarrukh25@gmail.com>
2018-07-04 22:03:57 +0100

    PruneQueueCommand now works. Also changed some experience requirements.

--- HashBot/src/main/java/me/afarrukh/hashbot/commands/music/PruneQueueCommand.java
+++ HashBot/src/main/java/me/afarrukh/hashbot/commands/music/PruneQueueCommand.java
@@ -5,8 +5,13 @@ import me.afarrukh.hashbot.commands.Command;
 import me.afarrukh.hashbot.core.Bot;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.entities.VoiceChannel;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 
 public class PruneQueueCommand extends Command {
 
@@ -17,11 +22,44 @@ public class PruneQueueCommand extends Command {
 
     @Override
     public void onInvocation(MessageReceivedEvent evt, String params) {
-        VoiceChannel voiceChannel = evt.getGuild().getAudioManager().getConnectedChannel();
 
-        for(AudioTrack track: Bot.musicManager.getGuildAudioPlayer(evt.getGuild()).getScheduler().getQueue()) {
-            String trackOwner = (String) track.getUserData();
+        List<Member> memberList = evt.getGuild().getAudioManager().getConnectedChannel().getMembers();
+
+        if(evt.getGuild().getAudioManager().getConnectedChannel() == null) {
+            evt.getTextChannel().sendMessage("The bot is not connected to voice.").queue();
+            return;
         }
+
+        Queue<AudioTrack> trackQueue = Bot.musicManager.getGuildAudioPlayer(evt.getGuild()).getScheduler().getQueue();
+        List<String> prunedUsers = new ArrayList<>();
+
+        int removedCount = 0;
+
+        for(AudioTrack track: trackQueue) {
+            String trackOwner = (String) track.getUserData();
+            boolean remove = false;
+
+            for(int i = 0; i<memberList.size(); i++) {
+                Member m = memberList.get(i);
+                if(m.getUser().getName().equals(trackOwner))
+                    break;
+                if(i == memberList.size()-1 && !m.getUser().getName().equalsIgnoreCase(trackOwner)) {
+                    trackQueue.remove(track);
+                    removedCount++;
+
+                    if(!prunedUsers.contains(trackOwner))
+                        prunedUsers.add(trackOwner);
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("Removed ").append(removedCount).append(" songs from the queue.\n");
+        if(!prunedUsers.isEmpty()) {
+            sb.append("From user(s): \n");
+            for (String s : prunedUsers)
+                sb.append(s).append("\n");
+        }
+        evt.getTextChannel().sendMessage(sb.toString().trim()).queue();
     }
 
     @Override
