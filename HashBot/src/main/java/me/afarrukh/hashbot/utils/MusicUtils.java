9c4a9f192a0e081ca1162f3e0d2fe8fd777108fa
afarrukh125 <33581953+afarrukh125@users.noreply.github.com>
2019-01-18 21:28:58 +0000

    Attempted System.gc() upon disconnect as previous fix did not work.

--- HashBot/src/main/java/me/afarrukh/hashbot/utils/MusicUtils.java
+++ HashBot/src/main/java/me/afarrukh/hashbot/utils/MusicUtils.java
@@ -62,9 +62,14 @@ public class MusicUtils {
         GuildMusicManager gm = Bot.musicManager.getGuildAudioPlayer(guild);
         if(gm.getPlayer().getPlayingTrack() != null)
             gm.getPlayer().getPlayingTrack().stop();
+        gm.getScheduler().getQueue().clear();
+        gm.getScheduler().setLoopingQueue(false);
+        gm.getScheduler().setLooping(false);
+        gm.getPlayer().setPaused(false);
+        gm.getDisconnectTimer().cancel();
         guild.getAudioManager().closeAudioConnection();
         gm.getPlayer().destroy();
-        Bot.musicManager.resetGuildAudioPlayer(guild);
+        System.gc();
     }
 
     /**
