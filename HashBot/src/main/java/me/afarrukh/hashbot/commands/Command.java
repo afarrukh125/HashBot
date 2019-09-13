b5151018df95aadaba1bb6489945f808cccf655b
afarrukh125 <33581953+afarrukh125@users.noreply.github.com>
2019-09-13 23:27:04 +0100

    Heavily document the Command class and begin working on allowing individual help for commands

--- HashBot/src/main/java/me/afarrukh/hashbot/commands/Command.java
+++ HashBot/src/main/java/me/afarrukh/hashbot/commands/Command.java
@@ -3,24 +3,118 @@ package me.afarrukh.hashbot.commands;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
+/**
+ * A command is something the user types to interact with the bot.
+ * This is usually prefixed with some character, in order for the bot to not call commands
+ * unexpectedly, for example, during normal conversation in a discord text channel.
+ *
+ * The default prefix for calling commands is !
+ * Assuming this is the prefix, to call a command, for example, to check the latency (which returns the latency in ms between the server
+ * that hosts this bot, and the discord servers), the "ping" command, can be called with
+ *
+ * <code>!ping</code>
+ */
 public abstract class Command {
 
     /**
      * The name of the command, i.e. what the user will need to type after the prefix to call it
      */
     private final String name;
-    private final Set<String> aliases = new HashSet<>();
+
+    /***
+     * The set of aliases, i.e. alternative ways to call this command
+     * The set constraint prevents a command from having duplicate aliases, although, at the moment,
+     * there is no way to prevent a command from having an alias the same as its original name
+     */
+    // TODO Add mechanism that prevents command from having an alias that is identical to the command name
+    private final Set<String> aliases;
+
+    /**
+     * A mapping between a parameter that is to be provided to this command, and a helpful description to help
+     * understand the purpose of the parameter.
+     *
+     * A parameter for a command, can be defined as any additional space-separated
+     * strings, that are to be used in the execution of the operation defined by said command.
+     *
+     * For instance, if there was a command to ban, and assuming our command prefix is !
+     *
+     * !ban foo
+     *
+     * Will take into account the parameter 'foo' when banning a user. For example, it may be that the user
+     * with user ID foo gets banned from the discord server that this <code>JDA</code> instance is in.
+     *
+     * Note that the command name, <i>ban</i>, in this case, is not a parameter.
+     */
+    private final Map<String, String> parameters;
+
+    /**
+     * A helpful description to describe the purpose of this command.
+     * @see me.afarrukh.hashbot.utils.EmbedUtils#getHelpMsg(MessageReceivedEvent, List)
+     */
     protected String description = null;
 
+    /**
+     * Create the command with the given name
+     *
+     * This is primarily what all the commands differ by semantically. This is not to be confused with
+     * the name of the class, although usually the name of the class typically ends up being either the
+     * same as the command name, or very similar.
+     *
+     * The command name is what the user types in order to invoke it. So in the example
+     *
+     * !ping
+     *
+     * 'ping' is the name of the command
+     *
+     * Note that there can be aliases added to a command, which, obviously enough, are alternate ways to call the same
+     * command. These are usually there for convenience.
+     * @param name The name of the command to be called.
+     * @see #addAlias(String)
+     */
+    // TODO Add some mechanism that ensures that no two commands have the same name
     public Command(String name) {
+        aliases = new HashSet<>();
+        parameters = new LinkedHashMap<>(); // We need to preserve the order of insertion of the arguments
         this.name = name;
     }
 
-    protected void addAlias(String s) {
-        aliases.add(s);
+    /**
+     * Adds an alias to this command
+     * @param alias An alternative name for this command
+     * @see #Command(String)
+     */
+    protected void addAlias(String alias) {
+        aliases.add(alias);
+    }
+
+    /**
+     * Add a parameter, and a statement describing this parameter ,to this command's parameter list
+     * @see #parameters
+     * @param parameter The name of the parameter
+     * @param description A meaningful description for the parameter
+     */
+    protected void addParameter(String parameter, String description) {
+        parameters.put(parameter, description);
+    }
+
+    /**
+     * Return an unmodifiable list of parameters for this command
+     * @return An unmodifiable list of parameters for this command
+     */
+    public List<String> getParameters() {
+        return Collections.unmodifiableList(new ArrayList<>(this.parameters.keySet()));
+    }
+
+    /**
+     * Obtain the help string, for use in commands that aim to seek the correct usage of this command
+     * This makes use of the parameters and their descriptions, as well as the overall command description
+     * @return A string with a usage, and each parameter description, as well as an overall description of this command
+     */
+    public String getHelpString() {
+        // TODO Complete this method
+        throw new UnsupportedOperationException();
     }
 
     /**
@@ -36,14 +130,26 @@ public abstract class Command {
      */
     public abstract void onIncorrectParams(TextChannel channel);
 
+    /**
+     * Obtain the name of this command
+     * @return The name of this command
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Obtain the set of aliases for this command
+     * @return The set of aliases for this command
+     */
     public Set<String> getAliases() {
-        return aliases;
+        return Collections.unmodifiableSet(aliases);
     }
 
+    /**
+     * Obtain the overall description for this command
+     * @return The string that contains a description of this command
+     */
     public String getDescription() {
         return description;
     }
