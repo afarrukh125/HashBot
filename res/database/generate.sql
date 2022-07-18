49d8223388138a17b482483ea49c8786c3428507
Abdullah <33581953+afarrukh125@users.noreply.github.com>
2022-07-18 20:51:22 +0100

    Add SQL database generation script

---
+++ res/database/generate.sql
@@ -0,0 +1,71 @@
+create table listtrack
+(
+    listid   INTEGER,
+    trackurl VARCHAR(100),
+    position INTEGER
+);
+
+create table listuser
+(
+    listid INTEGER
+        constraint listuser_pk
+            primary key autoincrement,
+    userid VARCHAR(60)
+);
+
+create table playlist
+(
+    listid INTEGER
+        constraint playlist_pk
+            primary key autoincrement,
+    name   VARCHAR(60),
+    userid VARCHAR(60)
+);
+
+create table sqlite_master
+(
+    type     TEXT,
+    name     TEXT,
+    tbl_name TEXT,
+    rootpage INT,
+    sql      TEXT
+);
+
+create table sqlite_sequence
+(
+    name,
+    seq
+);
+
+create table track
+(
+    url  VARCHAR(100),
+    name VARCHAR(100)
+);
+
+create table trackuser
+(
+    listid VARCHAR(60)
+        references playlist
+            on update cascade on delete cascade,
+    uri    VARCHAR(200),
+    userid VARCHAR(100)
+);
+
+create table user
+(
+    id     VARCHAR(60),
+    exp    VARCHAR(60),
+    level  INTEGER,
+    time   VARCHAR(60),
+    credit VARCHAR(60),
+    guild  VARCHAR(60)
+);
+
+create table username
+(
+    name VARCHAR(60),
+    id   VARCHAR(60)
+);
+
+
